259aa51f5ec51a33621b33fed099b599375b9443
Arvesh <arvesh9@gmail.com>
2012-09-09 09:31:21 +0400

    codeigniter configured

--- dental/application/config/constants.php
+++ dental/application/config/constants.php
@@ -36,6 +36,15 @@ define('FOPEN_READ_WRITE_CREATE',				'a+b');
 define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
+/*
+|--------------------------------------------------------------------------
+| DATE/TIME CONSTANTS
+|--------------------------------------------------------------------------
+|
+| These modes are used when working with date/time
+|
+*/
+define('TODAY', date('Y-m-d H:i:s'));
 
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
